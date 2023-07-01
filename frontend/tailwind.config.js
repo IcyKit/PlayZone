7764f446278126ce55c387af5c3eced305fa7d29
IcyKitMac <icykitdesign@gmail.com>
2023-07-01 22:10:49 +0300

    Feat: post component has been realised

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,15 +2,6 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      "button-bg": "#4574CF",
-      "header-bg-dark": "#26282B",
-      "button-dark": "#2C2C2C",
-      "contrast-dark": "#3A3D40",
-      "text-dark": "#CACCCF",
-      "text-contrast-dark": "#9C9EA1",
-      "main-bg-dark": "#161617",
-    },
     extend: {
       dropShadow: {
         "3xl": "0 35px 35px rgba(0, 0, 0, 0.25)",
